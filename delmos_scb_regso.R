624a1ab56905de7f2dd4a15c300a35fdd3d52f97
soleildeminuit <anders.elias@gmail.com>
2022-07-08 10:43:40 +0200

    Add socio economic index.

--- delmos_scb_regso.R
+++ delmos_scb_regso.R
@@ -1,14 +1,11 @@
-list.of.packages <- c("pxweb", "openxlsx")
+# list.of.packages <- c("pxweb", "openxlsx")
 new.packages <- list.of.packages[!(list.of.packages %in%
                                      installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
-library(pxweb)
 library(dplyr)
+library(jsonlite)
 library(sf)
-library(tmap)
-library(openxlsx)
-library(viridis)
 
 if (!file.exists("data/RegSO_2018.shp")){
   # url_deso <- "https://geodata.scb.se/geoserver/stat/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=stat%3ADeSO.2018&outputFormat=SHAPE-ZIP&format_options=charset:UTF-8"
@@ -34,6 +31,17 @@ for (i in 1:nrow(kommuner)){
           kommuner[i,]$kommun, sep = "")
   x <- jsonlite::fromJSON(delmos_url) %>% select(-deso)
   x <- x %>% rename(regsokod_delmos = regsokod) %>% select(-c(id))
+  
+  l2 <- list()
+  for (j in 1:nrow(x)){
+    delmos_omr_url <- paste("https://segregationsbarometern.delmos.se/api.php?action=get_region&region=regso&code=",
+                            x[j,]$regsokod_delmos,"&year=2020",sep="")
+    omr <- fromJSON(delmos_omr_url, flatten = TRUE)
+    omr_data <- data.frame(t(unlist(omr$meta)))
+    l2[[j]] <- omr_data
+  }
+  x <- cbind(x, do.call("rbind", l2))
+  
   l[[i]] <- x
 }
 df_delmos <- do.call("rbind", l) %>% rename(regso = regso_namn, kommun = kopplad_kommun)
@@ -42,4 +50,6 @@ regso <- inner_join(regso, df_delmos, by = c("regso" = "regso", "kommun" = "komm
 
 regso <- regso %>% select(uuid, regsokod, regsokod_delmos, regso, everything())
 
-st_write(regso, "data/RegSO_2018_Delmos.gpkg", delete_dsn = TRUE)
+regso <- regso %>% mutate_at(vars(antal_deso:omradestyp_kommun_5),list(as.numeric))
+
+# st_write(regso, "data/RegSO_2018_Delmos.gpkg", delete_dsn = TRUE)
