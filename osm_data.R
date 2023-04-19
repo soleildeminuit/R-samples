3f18c287b309b168b7f11511da99c80aa89c9f84
soleildeminuit <anders.elias@gmail.com>
2023-04-19 22:59:33 +0200

    Bug fix.

--- osm_data.R
+++ osm_data.R
@@ -1,9 +1,24 @@
+for (package in c(
+  "sf", 
+  "osmdata", 
+  "dplyr",
+  "jsonlite",
+  "tmap",
+  "viridis")) {
+  if (!require(package, character.only=T, quietly=T)) {
+    suppressPackageStartupMessages(package)
+    suppressWarnings(package)
+    install.packages(package)
+    library(package, character.only=T)
+  }
+}
+
 library(osmdata)
 library(sf)
 library(dplyr)
 library(tmap)
 
-sdn <- st_read("../data/sbk/Adm_area_ny.shp") %>% 
+sdn <- st_read("data/Adm_area_ny.shp") %>% 
   st_union() %>% st_as_sf() %>% st_transform(crs = 4326)
 
 q0 <- opq(bbox = st_bbox(sdn))
@@ -16,10 +31,13 @@ p$name <- iconv(p$name, "UTF-8")
 p <- p %>% dplyr::select(osm_id, name)
 # st_write(p, "parker.shp", delete_dsn = T)
 
-mapview::mapview(p)
+p <- p %>% mutate(area = st_area(.), area = as.numeric(area))
 
-t <- tm_shape(p %>% mutate(a = st_area(.))) + 
-  tm_fill("a", palette = "Greens", style = "jenks", alpha = 0.2) +
+t <- tm_shape(p) + 
+  tm_fill("area", palette = "Greens", style = "jenks", alpha = 0.7) +
   tm_borders()
 
-tmap_save(t, "t.html", width = 297, height = 210, units ="mm", dpi=300)
+tmap_mode("view")
+
+t
+# tmap_save(t, "t.html", width = 297, height = 210, units ="mm", dpi=300)
